11b15ca372fc43f756421d9c4ba145175bf0aff8
aekkakti <music.tomsk1@gmail.com>
2024-01-31 10:58:00 +0700

    Практическая работа №8 (Создание нового компонента product-details, визуализирующий информацию о товаре)

--- js/main.js
+++ js/main.js
@@ -1,53 +1,118 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+   <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+            
+            <product-details :details="details"></product-details>
+            
+            <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+            ></div>
+        </div>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+
+        <button
+                v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }"
+        >
+            Add to cart
+        </button>
+    </div>
+ `,
+    data() {
+        return {
+                product: "Socks",
+                brand: 'Vue Mastery',
+                selectedVariant: 0,
+                altText: "A pair of socks",
+                details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+                variants: [
+                    {
+                        variantId: 2234,
+                        variantColor: 'green',
+                        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                        variantQuantity: 10
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: 'blue',
+                        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                        variantQuantity: 0
+                    }
+                ],
+                cart: 0
+            }
+        },
+            methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct(index)
+            {
+                this.selectedVariant = index;
+            }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image() {
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            shipping() {
+                if (this.premium) {
+                    return "Free";
+                }
+                else {
+                    return 2.99;
+                }
+            }
+        }
+})
+
+
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-        ],
-        cart: 0,
-        onSale: true
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' на распродаже'
-            }
-            else {
-                return this.brand + ' ' + this.product + ' не на распродаже'
-            }
-        }
+        premium: true
     }
 })
