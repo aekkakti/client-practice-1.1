03a895ce756641e162bb3d22eb8e9c87f4f5b318
aekkakti <music.tomsk1@gmail.com>
2024-01-31 09:18:45 +0700

    Практическая работа №1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let app = new Vue ({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "../assets/vmSocks-green-onWhite.jpg",
+        description: "A pair of warm, fuzzy socks"
     }
 })
